f46e5e7114534710ec33892082ea6d337579c535
sahildubeydev <dsahil.contact@gmail.com>
2023-05-06 01:06:58 +0530

    Added routes, and landing page done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Categories from "./pages/categories/Categories";
+import ProductDetail from "./pages/productDetail/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Categories />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
